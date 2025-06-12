d1de3d89678ec2b62087a52cae87d52e631447b1
Mikhail Lisitski <limpid86@icloud.com>
2025-06-13 00:04:34 +0200

    adjust flow

--- main.tf
+++ main.tf
@@ -10,12 +10,12 @@ provider "aws" {
 #   }
 # }
 
- import {
-   id = "vpc-06bb7522670dea357"
-   to = aws_default_vpc.default
- }
- 
- resource "aws_default_vpc" "default" {
+import {
+  id = "vpc-06bb7522670dea357"
+  to = aws_default_vpc.default
+}
+
+resource "aws_default_vpc" "default" {
   tags = {
     Name = "Default VPC"
   }
